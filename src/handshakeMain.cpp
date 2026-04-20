53053bf70fea53f96aa73119922240844cb100c2
MichielN19 <Michielmitsjol@gmail.com>
2026-04-20 19:29:52 +0200

    WIP: Hash shared secret method in CryptoUtils

--- src/handshakeMain.cpp
+++ src/handshakeMain.cpp
@@ -1,5 +1,7 @@
 #include "DiffieHellman.h"
+#include "CryptoUtils.h"
 #include <iostream>
+#include <iomanip>
 
 int main() {
     DiffieHellman Alice;
@@ -8,18 +10,27 @@ int main() {
     long long A = Alice.getPublicKey();
     long long B = Bob.getPublicKey();
 
-    long long secretAlice = Alice.computeSharedSecret(B);
-    long long secretBob = Bob.computeSharedSecret(A);
+    unsigned long long secretAlice = Alice.computeSharedSecret(B);
+    unsigned long long secretBob = Bob.computeSharedSecret(A);
 
     if (secretAlice == secretBob) {
         std::cout << "\nBoth keys are the same\n";
         std::cout << "Alice: " << secretAlice << "\n";
         std::cout << "Bob: " << secretBob << "\n";
-        return 0;
+    } else {
+        std::cout << "\nBoth keys are different\n";
+        std::cout << "Alice: " << secretAlice << "\n";
+        std::cout << "Bob: " << secretBob << "\n";
     }
-    std::cout << "\nBoth keys are different\n";
-    std::cout << "Alice: " << secretAlice << "\n";
-    std::cout << "Bob: " << secretBob << "\n";
+
+    std::cout << secretBob << std::endl;
+
+    CryptoUtils::Key hashed_key = CryptoUtils::deriveKey(secretBob);
+    std::cout << "Derived key: ";
+    for (unsigned char byte : hashed_key) {
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)byte;
+    }
+    std::cout << std::dec << std::endl;
 
     return 0;
 }
